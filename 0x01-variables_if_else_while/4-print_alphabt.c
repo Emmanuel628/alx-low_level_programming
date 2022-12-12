c2ff6751cfbb5008f79ee7678b8a00a0f86ae6bd
Emmanuel628 <nwabudikeemmanuel30@gmail.com>
2022-12-12 07:21:55 -0800

    print alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets in lower case follow by new line
+ * Return: 0
+ */
+int main(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+	{
+		if (la != 'e' && la != 'q')
+			putchar(la);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
